68bc3d47f59cf96ea8efe6661e7b926ee8c41e32
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2024-10-27 15:50:57 +0100

    feat: working animation on walking

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
+    private Animator animator;
+
+    void Start()
+    {   
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -31,9 +38,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
         moveDirection = new Vector2(x, y).normalized;
     }
 
-
     void Move()
     {
+        //! Set "isWalking" to true if there is movement, otherwise set to false
+        if (moveDirection != Vector2.zero)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", moveDirection.x);
+            animator.SetFloat("InputY", moveDirection.y);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
         rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
